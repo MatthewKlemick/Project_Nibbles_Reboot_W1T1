feb8a2c720c4ec75504f3e81711b54a6298a9673
MatthewK <matt.klemick@xtra.co.nz>
2021-02-19 12:46:02 +1300

    push2

--- Nibbles_Reboot.py
+++ Nibbles_Reboot.py
@@ -5,7 +5,9 @@ import time
 
 pygame.init()
 pygame.font.init()
-screansize = (500,500)
+screanhight = 500
+screanwith = 500
+screansize = (screanwith,screanhight)
 win = pygame.display.set_mode(screansize)
 pygame.display.set_caption("Nibbles Reboot")
 x = 250
@@ -13,19 +15,24 @@ y = 250
 width = 20
 height = 20
 vel = 5
-score = 0
+score = 4
 gruning = True
 faceing = 0
 nibbleX = 0
 nibbleY = 0
 snakeL = 3
-snakePX = [250,250,250,250]
-snakePY = [250,250,250,250]
+snakePX = [250]
+snakePY = [250]
+objX = [0]
+objY = [0]
+objx3 = 0
+objy3 = 0
+objs = False
 gamestate = 1
 score_text = "score ="
 nibble_Active = False
-
-
+nibbles_image = pygame.image.load('swerl.png')
+#(255,255,0)
 
 def RenderText(text,x,y,TXTcolor=(255,255,255),size=30,):
 
@@ -35,12 +42,16 @@ def RenderText(text,x,y,TXTcolor=(255,255,255),size=30,):
 
 def tale():
     for x in range(len(snakePX)):
-        pygame.draw.rect(win,(255,255,0), (snakePX[x], snakePY[x], width, height))
+        snake = pygame.draw.rect(win,(255,255,0), (snakePX[x], snakePY[x], width, height))
+        
     if snakeL <= len(snakePX):
         snakePX.pop()
         snakePY.pop()
 
-    
+def r(screanh,screanw):
+    x2 = random.randrange(10, screanw - 30, 10)
+    y2 = random.randrange(10, screanh - 30, 10)
+    return  x2 , y2
 
 while gruning:
     keys = pygame.key.get_pressed()  
@@ -51,10 +62,11 @@ while gruning:
             
     win.fill((0,0,0))
     if  gamestate == 1:
-        RenderText("Nibbles",100,100,(255,255,255),80)
-        RenderText("Press any key to start",100,200,(255,255,255),30)
+        RenderText("Nibbles",100,50,(255,255,255),80)
+        win.blit(nibbles_image, (100,150))
+        RenderText("Press any key to start",100,100,(255,255,255),30)
         pressed = keys
-        event = pygame.event.wait()
+
 
         if (event.type == pygame.KEYDOWN) or (event.type == pygame.MOUSEBUTTONDOWN):
             gamestate = 2
@@ -62,14 +74,6 @@ while gruning:
     elif gamestate == 2:
         win.fill((25,25,255))
 
-        if snakePX[0] == 470:
-            gamestate = 3
-        elif snakePY[0] == 470:
-            gamestate = 3
-        elif snakePX[0] == 10:
-            gamestate = 3
-        elif snakePY[0] == 10:
-            gamestate = 3
 
         if keys [pygame.K_LEFT]and not faceing == 2:
             faceing = 1
@@ -85,41 +89,71 @@ while gruning:
             snakePY.insert(0,y)
             x -= vel
         elif faceing == 2:
-            snakePX.insert(0,x + vel)
+            snakePX.insert(0,x + vel )
             snakePY.insert(0,y)
             x += vel
         elif faceing == 3:
             snakePX.insert(0,x)
-            snakePY.insert(0,y - vel)
+            snakePY.insert(0,y - vel )
             y -= vel
         elif faceing == 4:
             snakePX.insert(0,x)
-            snakePY.insert(0,y + vel)
+            snakePY.insert(0,y + vel )
             y += vel
-    
+
         tale()
+
+        
+        if snakePX[0] > 470 :
+            gamestate = 3
+        elif snakePY[0] > 470:
+            gamestate = 3
+        elif snakePX[0] < 10:
+            gamestate = 3
+        elif snakePY[0] < 10:
+            gamestate = 3
 
         pygame.draw.rect(win,(255,128,0), (0, 0, 500, 10))
         pygame.draw.rect(win,(255,128, 0), (0, 490, 500, 10))
         pygame.draw.rect(win,(255,128, 0), (0, 0, 10, 500))
         pygame.draw.rect(win,(255,128, 0), (490, 0, 10, 500))
-        
+
         if nibble_Active == True:
             pygame.draw.rect(win,(255, 255, 255), (nibbleX, nibbleY , width, height))
-            if x <=  nibbleX and x >=  (nibbleX - 20) and y <= nibbleY  and y >= (nibbleY - 20):
+            if abs(snakePX[0] - nibbleX) <= 20  and abs(snakePY[0] - nibbleY) <= 20:
+                
                 snakeL = snakeL + 1
                 vel = vel + 1
                 score = score + 1
                 nibble_Active = False
 
         else:
-            nibbleX = random.randrange(10, 470, 1)
-            nibbleY = random.randrange(10, 470, 1)
+            nibbleX , nibbleY = r(screanwith,screanhight)
             nibble_Active = True
 
         pygame.draw.rect(win,(0, 0, 0), (0, 0, 90, 30))
         score_text = "score = " + str(score)
         RenderText(score_text,0,0)
+
+        if len(objX) == 3:
+            for v in range(len(objX)):
+                pygame.draw.rect(win,(255,128,0), (objX[v], objY[v], width, height))
+                if abs(snakePX[0] - objX[v]) <= 20  and abs(snakePY[0] - objY[v]) <= 20:
+                  gamestate = 3  
+
+        if score >= 5 and objs == False:
+            objX.pop(0)
+            objY.pop(0)
+            objx3 , objy3 = r(screanwith,screanhight)
+            objX.append(objx3)
+            objY.append(objy3)
+            objx3 , objy3 = r(screanwith,screanhight)
+            objX.append(objx3)
+            objY.append(objy3)
+            objx3 , objy3 = r(screanwith,screanhight)
+            objX.append(objx3)
+            objY.append(objy3)
+            objs = True
 
     elif gamestate == 3:
 
@@ -133,14 +167,18 @@ while gruning:
 
         if (event.type == pygame.KEYDOWN) or (event.type == pygame.MOUSEBUTTONDOWN):
             nibble_Active = False
+            objs = False
+            objX = []
+            objY = []
             x = 250
             y = 250
+            vel = 5
             faceing = 0
             nibbleX = 0
             nibbleY = 0
             snakeL = 3
-            snakePX = [250,250,250,250]
-            snakePY = [250,250,250,250]
+            snakePX = [250]
+            snakePY = [250]
             score = 0
             gamestate = 2
 
